175c6fb12ea9d5464cf4b213b3e248956456a019
nwxanin <120450273+nwxanin@users.noreply.github.com>
2022-12-29 23:58:31 +0330

    updated date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function formatDate(timetamp) {
+  let date = new Date(timetamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  let day = days[date.getDay()];
+  return `${day}<br/>${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -13,6 +28,9 @@ function displayTemperature(response) {
 
   let windSpeedElement = document.querySelector("#wind-speed");
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiUrl =
